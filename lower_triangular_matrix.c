140e6750592f8d53e72aaeb90d00ccdf02a23d05
Code 4 U <72220088+codejust4U@users.noreply.github.com>
2023-05-04 15:43:45 +0530

    Create lower_triangular_matrix.c

---
+++ lower_triangular_matrix.c
@@ -0,0 +1,45 @@
+//WAP for finding lower triangular matrix
+//Coder: Pankaj Kori
+//Date: 22-08-2022 
+#include <stdio.h>
+void main()
+{
+    system("cls");
+    int i, j, r, c, array[10][10];
+    printf("Enter the rows and columns:\n");
+    scanf("%d %d", &r, &c);
+    for (i = 1; i <= r; i++)
+    {
+        for (j = 1; j <= c; j++)
+        {
+            printf("array[%d][%d] = ", i, j);
+            scanf("%d", &array[i][j]);
+        }
+    }
+    printf("Matrix is\n");
+    for (i = 1; i <= r; i++)
+    {
+        for (j = 1; j <= c; j++)
+        {
+            
+            printf("%d",array[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n\nLower triangular matrix is");
+    for (i = 1; i <= r; i++)
+    {
+        printf("\n");
+        for (j = 1; j <= c; j++)
+        {
+            if (i >= j)
+            {
+                printf("%d", array[i][j]);
+            }
+            else
+            {
+                printf("\t");
+            }
+        }
+    }
+}
