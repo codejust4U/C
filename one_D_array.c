9b89d26c9333b43d2f9efaeb3df1f330d0f794bb
Code 4 U <72220088+codejust4U@users.noreply.github.com>
2023-05-06 13:03:49 +0530

    Create one_D_array.c

---
+++ one_D_array.c
@@ -0,0 +1,21 @@
+//Again writer Pankaj Kori
+#include<stdio.h>
+#define size 10
+void main()
+{
+    system("cls");
+     int a[10],i;
+    printf("Enter the numbers\n");
+    for ( i = 0; i < size; i++)
+    {
+        /* code */
+        scanf("%d",&a[i]);
+    }
+    printf("The numbers are :-\n");
+    for ( i = 0; i < size; i++)
+    {
+        /* code */
+         printf("%d \t",a[i]);
+    }
+   
+}
